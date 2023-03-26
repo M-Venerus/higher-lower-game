214fc23e0d2ab0330bcc3d94d15223fb4fb30e33
M-Venerus <mikey.motorist@outlook.com>
2023-03-26 15:49:32 -0400

    Add docstrings and rename variables with names that were unrepresentative of their values.

--- higher-lower.py
+++ higher-lower.py
@@ -2,15 +2,18 @@ from assets import data
 from random import randint
 
 def greeting():
+    """Generate initial game greeting."""
     print("Welcome to celebrity guessing game!\n\nYou have to guess which celebrity has more followers!")
 
 def selection():
+    """Select and return a random dictionary from data list. Removes the dictionary from the list once selected."""
     data_index = randint(0, len(data) - 1)
     celeb = data[data_index]
     data.pop(data_index)
     return celeb
 
 def compare(celeb_1, celeb_2):
+    """Compare follower count between celebrity 1 and celebrity 2, and return appropriate result."""
     print(f"Celebrity 1: {celeb_1['name']}, a {celeb_1['description']}, from {celeb_1['country']}.")
     print("VS")
     print(f"Celebrity 2: {celeb_2['name']}, a {celeb_2['description']}, from {celeb_2['country']}.")
@@ -22,6 +25,7 @@ def compare(celeb_1, celeb_2):
         return 2
 
 def guess(celeb_better):
+    """Prompt user for input of guess for celebrity with more followers and compare the guess. Return True or false based on if user is correct."""
     user_guess = int(input("Which celebrity has more followers? Type '1' or '2': "))
     if celeb_better == 0:
         return True
@@ -31,17 +35,20 @@ def guess(celeb_better):
         return False 
 
 def display_score(score, celeb_1, celeb_2):
+    """Notify user of their correct answer along with information pertaining to why (celebrity follower numbers). Update score and print new score."""
     print(f"\nYou're right! {celeb_1['name']} has {celeb_1['follower_count']},000 followers, while {celeb_2['name']} has {celeb_2['follower_count']},000 followers.")
     score += 1
     print(f"Current score: {score}.")
     return 1
 
 def end_game(score, celeb_1, celeb_2):
+    """Notify user of their incorrect answer along with information pertaining to why (celebrity follower numbers). Display final score."""
     print(f"\nSorry, that was incorrect. {celeb_1['name']} has {celeb_1['follower_count']},000 followers, while {celeb_2['name']} has {celeb_2['follower_count']},000 followers.")
     print(f"Final score = {score}.")
     
 
 def main():
+    """Main function to run game."""
     greeting()
     celeb_1 = selection()
     game_running = True
@@ -50,8 +57,8 @@ def main():
     while game_running:
         celeb_2 = selection()
         celeb_better = compare(celeb_1, celeb_2)
-        right_or_wrong = guess(celeb_better)
-        if right_or_wrong:
+        user_is_right = guess(celeb_better)
+        if user_is_right:
             score += display_score(score, celeb_1, celeb_2)
             celeb_1 = celeb_2
         else:
